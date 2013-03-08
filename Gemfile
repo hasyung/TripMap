3ebe5dabb35ea92c6d0fc2ffa808125b843fa705
ChuckLee <sapronlee@gmail.com>
2013-03-08 15:47:02 +0800

    第一次提交项目

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'http://ruby.taobao.org/'
+
+gem 'rails', '3.2.10'
+gem 'mysql2'
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'quiet_assets'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'spork-rails'
+  gem 'capybara'
+  gem 'thin'
+  gem 'pry'
+  gem 'pry-doc'
+  gem 'pry-nav'
+  gem 'pry-remote'
+  gem 'pry-rails'
+  gem 'simplecov'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripMap::Application.config.secret_token = '44fbbf4ccba641aaa4391b8c98a6efd37145e494242495dcc7745366d2a076c697a87bd8412444a9de5a5ebef61ae833ffb102224ba591f59a39be00834b7232'
