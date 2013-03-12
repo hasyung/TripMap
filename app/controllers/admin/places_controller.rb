d6238f44205ff4b79747e410b0fbe78965a8fec2
joseen <qs.joseen@gmail.com>
2013-03-13 00:37:21 +0800

    添加编辑景点

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -17,13 +17,20 @@ class Admin::PlacesController < Admin::ApplicationController
 
   def edit
     @place = Place.find params[:id]
+    @place_icon  = @place.place_icon
+    @place_image = @place.place_image
+    @place_description_image = @place.place_description_image
+    @place_audio = @place.place_audio
+    @place_video = @place.place_video
+    @place_description = @place.place_description
     add_breadcrumb :edit
   end
 
   def create
     @place = Place.new params[:place]
+    binding.pry
     if @place.save
-      redirect_to admin_places_path, notice: ''
+      redirect_to admin_places_path, notice: t('messages.places.success')
     else
       redirect_to :new
     end
@@ -32,18 +39,18 @@ class Admin::PlacesController < Admin::ApplicationController
   def update
     @place = Place.find params[:place]
     if @place.update_attributes
-      redirect_to admin_places_path, notice: ''
+      redirect_to admin_places_path, notice: t('messages.places.success')
     else
       redirect_to :edit
     end
   end
 
   def destroy
-    @place = Place.find params[:place]
+    @place = Place.find params[:id]
     if @place.destroy
-      redirect_to admin_places_path, notice: ''
+      redirect_to admin_places_path, notice: t('messages.places.success')
     else
-      redirect_to admin_places_path, notice: ''
+      redirect_to admin_places_path, notice: t('messages.places.error')
     end
   end
 end
