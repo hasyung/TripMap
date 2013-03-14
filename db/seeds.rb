6b7a30e659a5eee73aec9c2fce9db135d761f142
zh.yang <howiloveyou@126.com>
2013-03-14 09:53:11 +0800

    1. Fixed seed error.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ yn = Province.new name: "云南", slug: "yn"
 yn.save
 
 lj = yn.maps.build name: "丽江", slug: "lj"
+lj.save
 
 lj.scenics.build name: "丽江市", slug: "ljs"
 lj.scenics.build name: "玉龙雪山", slug: "ylxs"
@@ -11,11 +12,13 @@ lj.scenics.build name: "玉龙雪山", slug: "ylxs"
 lj.places.build name: "束河古镇", slug: "shgz"
 lj.places.build name: "拉市海", slug: "lsh"
 
+lj.save
+
 cmw = lj.recommends.build name: "吃美味", slug: "cmw"
 zkz = lj.recommends.build name: "住客栈", slug: "zkz"
 dhj = lj.recommends.build name: "带回家", slug: "dhj"
 
-lj.save
+cmw.save
 
 cmw.recommend_records.build name: "丽江小吃"
 cmw.recommend_records.build name: "地道菜"
