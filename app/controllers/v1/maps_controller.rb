61527b24b569267041e5fe198882ae33a3c323f3
hasyung <289170301@qq.com>
2013-03-21 17:15:15 +0800

    修正api错误

--- app/controllers/v1/maps_controller.rb
+++ app/controllers/v1/maps_controller.rb
@@ -14,6 +14,7 @@ class V1::MapsController < V1::ApplicationController
                                    :recommends_count => map.recommends_count,
                                    :shares_count => map.shares_count,
                                    :infos_count => map.infos_count
+                                   
                                  }
     		end
     		render :json => result
