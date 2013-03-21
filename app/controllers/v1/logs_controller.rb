b17d2289e1f935f8cdd26fc32665cac19d3b315a
hasyung <289170301@qq.com>
2013-03-21 15:22:35 +0800

    修正api错误

--- app/controllers/v1/logs_controller.rb
+++ app/controllers/v1/logs_controller.rb
@@ -1,7 +1,7 @@
 class V1::LogsController < V1::ApplicationController
 
 	def create
-		activate_map = ActivateMap.find {|a| a.device_id == params[:device_id].to_i}
+		activate_map = ActivateMap.find {|a| a.device_id == params[:device_id]}
 		activate_map.logs.new map_id: params[:map_id].to_i,
 														 device_type_cd: params[:device_type].to_i,
 														 slug: params[:slug].downcase,
