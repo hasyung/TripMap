9acee450bcf9ab893d291814c9d0c45b25e41830
hasyung <289170301@qq.com>
2013-03-22 12:01:14 +0800

    删除多余api文件

--- app/controllers/v1/weathers_controller.rb
+++
@@ -1,9 +0,0 @@
-class V1::WeathersController < V1::ApplicationController
-
-	def index
-		@map = Map.find params[:map_id]
-		weathers = Weather::API.get_suit_weather(@map.name)
-		render :json => weathers
-	end
-
-end
