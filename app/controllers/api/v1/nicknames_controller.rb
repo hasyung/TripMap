1937d628125dc786ada97ba76953d242604728a6
hasyung <289170301@qq.com>
2013-04-01 17:45:03 +0800

    修正昵称api错误

--- app/controllers/api/v1/nicknames_controller.rb
+++ app/controllers/api/v1/nicknames_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::NicknamesController < Api::V1::ApplicationController
 		result = {}
 		serial = MapSerialNumber.find{|num| num.code == params[:serial]}
 		if serial.present? && serial.nickname.present?
-      		result << {nickname: serial.nickname.nickname}
+      		result = {id: serial.nickname.id, nickname: serial.nickname.nickname}
     		end
     		render :json => result
 	end
@@ -11,7 +11,7 @@ class Api::V1::NicknamesController < Api::V1::ApplicationController
 	def create
 		result = {result: false}
 		serial = MapSerialNumber.find{|num| num.code == params[:serial]}
-		if serial.present? && Nickname.find{|n| n.nickname == params[:nickname]}.blank?
+		if serial.present? && serial.nickname.blank? && Nickname.find{|n| n.nickname == params[:nickname]}.blank?
 			serial.build_nickname nickname: params[:nickname]
 			if serial.save
 				result = {result: true}
