cba4fe2c1c1dce12038ea2868a22405525be45d3
hasyung <289170301@qq.com>
2013-04-02 19:02:12 +0800

    修改昵称nickname为name

--- app/models/nickname.rb
+++ app/models/nickname.rb
@@ -1,10 +1,10 @@
 class Nickname < ActiveRecord::Base
   # attr_accessible
-  attr_accessible :map_serial_number_id , :nickname
+  attr_accessible :map_serial_number_id , :name
 
   belongs_to :map_serial_number
   has_many :shares, :dependent => :destroy
 
 
-  validates :nickname, :presence => true, :length => { :within => 0..30 }
+  validates :name, :presence => true, :length => { :within => 0..30 }
 end
