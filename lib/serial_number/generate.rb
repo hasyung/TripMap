a9e172be27e614a62aec006ba7fda4dd4f5f2253
ChuckLee <sapronlee@gmail.com>
2013-04-09 11:32:46 +0800

    修改序列号的算法

--- lib/serial_number/generate.rb
+++ lib/serial_number/generate.rb
@@ -1,18 +1,18 @@
 module SerialNumber
   module Generate
     
-    def create_single_sn serial_type = 0
+    def create_single_sn serial_type = 0, map_id = 99, order = 1
       use_count = generate_use_count serial_type
-      result = generate_random_array
+      result = format_by generate_random_array
       fill_serial_type result, serial_type
-      fill_map_province result, self.province_id
-      fill_map_id result, self.id
+      fill_map_id result, map_id
+      fill_order result, order
       # model save
       self.map_serial_numbers.build code: result.join, type_cd: serial_type, count: use_count, printed_cd: 0
       self.save
     end
     
-    private
+    # private
     
     def generate_random_array
       result = Random.new_seed.to_s.split(//).shuffle!.sample(11).shuffle!
@@ -22,10 +22,27 @@ module SerialNumber
       result
     end
     
+    def format_by array = []
+      [] unless array.is_a?(Array)
+      array.insert(3, nil).insert(6, nil).insert(9, nil).insert(11, nil).insert(14, nil) unless array.include?(nil)
+      array
+    end
+    
+    def fill_order result, order
+      num = case order
+      when 1..9
+        ['0', order]
+      when 10..99
+        [order.to_s.first, order.to_s.last]
+      else
+        ['0', '0']
+      end
+      result[9] = num.last if result[9].blank?
+      result[14] = num.first if result[14].blank?
+    end
+    
     def fill_serial_type result, serial_type
       num = case serial_type
-      when MapSerialNumber.types[:free]
-        [1]
       when MapSerialNumber.types[:ordinary]
         (2..5).to_a
       when MapSerialNumber.types[:favorite]
@@ -33,7 +50,7 @@ module SerialNumber
       else
         [0]
       end
-      result.insert(1, num.sample(1).first.to_s)
+      result[3] = num.sample(1).first.to_s if result[3].blank?
     end
     
     def fill_map_id result, map_id
@@ -45,7 +62,8 @@ module SerialNumber
       else
         ['0', '0']
       end
-      result.insert(8, num.first).insert(12, num.last)
+      result[6] = num.last if result[6].blank?
+      result[11] = num.first if result[11].blank?
     end
       
     def fill_map_province result, province_id
@@ -65,7 +83,7 @@ module SerialNumber
       when MapSerialNumber.types[:free]
         1
       when (MapSerialNumber.types[:ordinary]..MapSerialNumber.types[:favorite])
-        5
+        10
       else
         0
       end
