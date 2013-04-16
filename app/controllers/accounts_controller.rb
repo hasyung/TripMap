bb8663e63c859d03b8abc9acd85ef07baf55a98c
hasyung <289170301@qq.com>
2013-04-16 10:25:15 +0800

    修正登录页一个错误

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,7 @@ class AccountsController < ApplicationController
   end
 
   def create
-    @account = Account.new params[:account]
+    (redirect_to new_account_path, :alert => t("messages.accounts.activate_error"); return) if params[:device_id].blank?
     serial_num = params[:account][:map_serial_number_attributes][:code]
     if serial_num.present?
       serial = MapSerialNumber.find{ |s| s.code == serial_num }
@@ -13,7 +13,7 @@ class AccountsController < ApplicationController
 
     activate_map = ActivateMap.find{ |o| o.device_id == params[:device_id] }
     activate_map = ActivateMap.create(device_id: params[:device_id]) if activate_map.blank?
-
+    @account = activate_map.accounts.new params[:account]
     if @account.save
       serial.account_id = @account.id
       serial.activate_cd = 1
