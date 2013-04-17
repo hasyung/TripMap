be2a63981e772c43f447dc82ed79497df193c76b
hasyung <289170301@qq.com>
2013-04-17 18:02:47 +0800

    登录页增加修改昵称和绑定序列号分支

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -10,11 +10,23 @@ class AccountsController < ApplicationController
       (redirect_to new_accounts_path(params[:device_id]), :alert => t("messages.accounts.serial_error"); return) if serial.blank? ||
                                                                                             serial.activate_cd == 1
     end
-
     activate_map = ActivateMap.find{ |o| o.device_id == params[:device_id] }
     activate_map = ActivateMap.create(device_id: params[:device_id]) if activate_map.blank?
-    @account = activate_map.accounts.new params[:account]
-    if activate_map.save
+    @account = Account.find_by_email params[:account][:email]
+    if @account.blank?
+      @account = activate_map.accounts.new params[:account]
+      result = activate_map.save
+    else
+       (redirect_to new_accounts_path(params[:device_id]), :alert => t("messages.accounts.password_error"); return) if 
+                                                                                            !@account.valid_password?(params[:account][:password])
+      @account.nickname = params[:account][:nickname]
+      @account.password = @account.password_confirmation = params[:account][:password]
+      result = @account.save
+      activate_map.accounts << @account if !activate_map.accounts.include?(@account)
+    end
+    if result
+      (redirect_to new_accounts_path(params[:device_id]), :alert => t("messages.accounts.re_serial_error"); return) if 
+                                                                                           @account.map_serial_number.present?
       if serial.present?
         serial.account_id = @account.id
         serial.activate_cd = 1
@@ -23,7 +35,7 @@ class AccountsController < ApplicationController
       redirect_to success_accounts_path
     else
       render :new
-    end
+    end 
   end
 
   def success
