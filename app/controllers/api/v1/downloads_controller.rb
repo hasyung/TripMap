4a88e7828ac5058d18ca81b788f9fc154ace49a9
zh.yang <howiloveyou@126.com>
2013-05-02 16:27:27 +0800

    1. Modify download code.

--- app/controllers/api/v1/downloads_controller.rb
+++ app/controllers/api/v1/downloads_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::DownloadsController < Api::V1::ApplicationController
-  
+
   def count
     d = Download.first
 
     if d.nil?
       entity = Download.new :count => 1
       entity.save
+    else
+      d.count = d.count + 1
+      d.save
     end
-
-    d.count += 1
-    d.save
 
     render :text => d.count.to_s
 
