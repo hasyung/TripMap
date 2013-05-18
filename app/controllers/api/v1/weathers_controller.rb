c3e340bd4b56556c065864fdd79a8ccc2eacf5b9
zh.yang <howiloveyou@126.com>
2013-05-18 11:36:28 +0800

    1. Setting rb file's encoding.

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'weather/api'
 
 class Api::V1::WeathersController < Api::V1::ApplicationController
@@ -5,10 +7,10 @@ class Api::V1::WeathersController < Api::V1::ApplicationController
   DAY_SECONDS = 24 * 60 * 60
 
   def index
-    weather = { 
+    weather = {
       tmp_current:  "20℃",  tmp_today:  " 16℃/30℃",
       tmp_desc:     "多云",  tmp_wind:   "南风 2级",
-      tmp_pic_from: 1, tmp_pic_to: 1,
+      tmp_pic_from: "1", tmp_pic_to: "1",
       tmp_humidity: "73%"
     }
 
