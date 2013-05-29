a1f4b59a7496b45cd523e6e49f1c9a1c6cf2423c
zh.yang <howiloveyou@126.com>
2013-05-29 19:26:30 +0800

    1. Fiexed device with map serial validate error.

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -14,9 +14,9 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
   end
 
   def validate
-    ret = { result: false }
+    ret = { result: "false" }
 
-    fields = [ 'device_id', 'serial' ]
+    fields = [ 'device_id', 'serial', 'map_id' ]
     ( render :json => ret.merge(set_msg("errors.api.invalid_params")); return ) if has_nil_value_in fields
 
     uuid = params[:device_id]
@@ -24,23 +24,26 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
     device = Device.create device_id: uuid if device.blank?
     ( render :json => ret.merge(set_msg("errors.api.devices.device_create_error")); return ) if device.blank?
 
-    serial = MapSerialNumber.where(:code => params[:serial]).first
-    (render :json => ret.merge(set_msg("errors.api.devices.serial_error")); return) if serial.blank?
-    (render :json => ret.merge(set_msg("errors.api.devices.ser_count_used_exp")); return) if serial.devices.count >= Setting.serial_devices_size.to_i
-    (render :json => ret.merge(set_msg("errors.api.devices.map_registered")); return) if get_maps_from_serials(device.map_serial_numbers).include?(serial.map_id)
+    has_serial = MapSerialNumber.where(:code => params[:serial]).first
+    ( render :json => ret.merge(set_msg("errors.api.devices.serial_error")); return ) if has_serial.blank?
 
-    device.map_serial_numbers << serial
-    if serial.activate_cd == 0
-      serial.activate_cd = 1
-      serial.save
+    match_map_serial = MapSerialNumber.where(:code => params[:serial], :map_id => params[:map_id]).first
+    (render :json => ret.merge(set_msg("errors.api.devices.serial_not_match_map")); return) if match_map_serial.blank?
+    (render :json => ret.merge(set_msg("errors.api.devices.ser_count_used_exp")); return) if match_map_serial.devices.count >= Setting.serial_devices_size.to_i
+    (render :json => ret.merge(set_msg("errors.api.devices.map_registered")); return) if get_maps_from_serials(device.map_serial_numbers).include?(match_map_serial.map_id)
+
+    device.map_serial_numbers << match_map_serial
+    if match_map_serial.activate_cd == 0
+      match_map_serial.activate_cd = 1
+      match_map_serial.save
     end
 
-    ret[:result] = true
+    ret[:result] = "true"
     render :json => ret.merge(set_msg("tips.api.success"))
   end
 
   def unbind_device
-    ret = { result: false }
+    ret = { result: "false" }
     fields = [ 'device_id' ]
     ( render :json => ret.merge(set_msg("errors.api.maps.device_id")); return ) if has_nil_value_in fields
 
@@ -48,7 +51,7 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
     ( render :json => ret.merge(set_msg("errors.api.maps.device_id")); return ) if d.nil?
 
     d.destroy
-    ret[:result] = true
+    ret[:result] = "true"
     render :json => ret.merge(set_msg("tips.api.success"))
   end
 
