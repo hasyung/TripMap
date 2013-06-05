080395c1634dbe6e07ce31c082d351bd3a181d68
zh.yang <howiloveyou@126.com>
2013-06-05 18:21:35 +0800

    1. Clear rails cache before write.

--- app/observer/trip_map_observer.rb
+++ app/observer/trip_map_observer.rb
@@ -49,6 +49,7 @@ class TripMapObserver < ActiveRecord::Observer
     map_instance = get_map(model)
     return nil if map_instance.nil?
 
+    Rails.cache.clear
     Rails.cache.write("map_#{map_instance.id}", map_instance.get_map_values)
 
     map_instance.version = Time.now.to_i
